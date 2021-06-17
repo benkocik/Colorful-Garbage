e209653be089c0cdb641c34231f31d2a6324156d
Ben Kocik <bkocik@heestand.tech>
2021-06-17 14:52:15 -0400

    Finialized steganography.py, added drawio files.

--- steganography.py
+++ steganography.py
@@ -8,28 +8,40 @@ from reference import messageToBinary
 import cv2
 import numpy as np
 import logging
+import sys
 
-def encode_text(image, message):
+# Constants for end of message types
+# Use 5 '#'s to signify end of string type message
+STRING_END = "#####"
+# Use 5 '$'s to signify end of file type message
+FILE_END = "$$$$$"
+
+def encode_text(image, message, messageType):
     '''
     Encodes specified message into provided image
 
             Parameters:
                 image: string file path of image that will have data encoded to it
                 message: string of text to encode into image
+                messageType: boolean to determine string or file. True for string, False for file
 
             Returns:
-                CV2 image with hidden message - convert with imwrite
+                im: CV2 image with hidden message - convert with imwrite
     '''
     im = cv2.imread(image)  # Read image
-    n_bytes = im.shape[0] * im.shape[1] * 3 // 8    # Calculate max bytes
-    message += "#####"  # Use 5 '#'s to signify end of message - this is standard for encoding and decoding
+    nBytes = im.shape[0] * im.shape[1] * 3 // 8    # Calculate max bytes
+    # Based on message type, add string to end
+    if messageType:
+        message += STRING_END
+    elif not messageType:
+        message += FILE_END
     # Check message can fit into image
-    if len(message) > n_bytes:
+    if len(message) > nBytes:
         logging.error("Message is too large or image is too small. The data will not fit into the image. Try a smaller message or larger image")
-        raise ValueError("Message too large or image too small, need bigger image or less data")
+        sys.exit(1)
 
     i = 0
-    bin_message = messageToBinary(message)  # Convert data to binary
+    binaryMessage = messageToBinary(message)  # Convert data to binary
 
     # Break image into values
     for values in im:
@@ -38,75 +50,106 @@ def encode_text(image, message):
             # Convert rgb of pixel into binary
             r, g, b = messageToBinary(pixel)
             # RED IN PIXEL
-            if i < len(bin_message):
+            if i < len(binaryMessage):
                 # Hide data into LSB
-                pixel[0] = int(r[:-1] + bin_message[i], 2)
+                pixel[0] = int(r[:-1] + binaryMessage[i], 2)
                 i += 1
             # GREEN IN PIXEL
-            if i < len(bin_message):
+            if i < len(binaryMessage):
                 # Hide data into LSB
-                pixel[1] = int(g[:-1] + bin_message[i], 2)
+                pixel[1] = int(g[:-1] + binaryMessage[i], 2)
                 i+= 1
             # BLUE IN PIXEL
-            if i < len(bin_message):
+            if i < len(binaryMessage):
                 # Hide data into LSB
-                pixel[2] = int(b[:-1] + bin_message[i], 2)
+                pixel[2] = int(b[:-1] + binaryMessage[i], 2)
                 i+=1
             # Data is fully encoded, leave for loop
-            if i >= len(bin_message):
+            if i >= len(binaryMessage):
                 break
-
-    return im
-
-
-def encode_file():
-    '''
     
-    '''
-    # TODO Encode file into image
+    return im   # Return cv2 image 
 
-def decode_text():
+def decode_text(image):
     '''
+    Decodes message from specified image file
 
-    '''
+            Parameters:
+                image: string file path of image that will have data encoded to it
 
-def decode_file():
+            Returns:
+                message: string message that was decoded from image
+                messageType: boolean message type. True for string, False for file
     '''
+    im = cv2.imread(image)  # Read image
+    binaryData = ""
+    # Break image into values
+    for values in im:
+        # Break values into pixels
+        for pixel in values:
+            # Convert rgb of pixel into binary
+            r, g, b = messageToBinary(pixel)
+            binaryData += r[-1] # Get LSB from red
+            binaryData += g[-1] # Get LSB from green
+            binaryData += b[-1] # Get LSB from blue
+    # Split into 8 bits - 8 bits in a byte
+    byteData = [binaryData[i: i+8] for i in range(0, len(binaryData), 8)]
+    message = ""
+    for byte in byteData:
+        message += chr(int(byte, 2))
+        # Check if string or file
+        if message[-5:] == STRING_END:
+            messageType = True
+            break
+        elif message[-5:] == FILE_END:
+            messageType = False
+            break
+        else:
+            logging.error("Message not found in image, try a different image")
+            sys.exit(1)
+
+    message = message[:-5]  # Remove ending from message
+    return message, messageType     # Return message as string and message type as boolean
     
-    '''
 
-
-def Steganography(argDict):
+def steganography(argDict, mode):
     '''
     Assigns inputs to variables. Begins steganography.
 
             Parameters:
                 argDict: dictionary of arguments provided
+                mode: boolean to decide encoding or decoding. True to encode, False to decode
 
             Returns:
-                Output file or revealed message
+                String path of output file
+                or (based on mode)
+                String message and boolean message type. True for message, False for file
     '''
     # Get values from argDict
     startImage = argDict['input']   # Image inputted
-    message = argDict['message']    # Message to hide TODO Check if empty
-    file_to_hide = argDict['file_to_hide'] # TODO Figure out what file_to_hide is and do check if empty
-    process = argDict['process']    # TODO Figure out what this is...
-    outFile = argDict['out']        # Image file to output to TODO may need to check if empty
+    message = argDict['message']    # Message to hide, either file or string
+    outFile = argDict['out']        # Image file to output to
 
     # Check data to determine what to do
-    if message != None:
+    if mode:
         # Hide message in image
-        logging.debug("Mode: Hiding text into image")
-        encodedImage = encode_text(startImage, message)
+        logging.debug("Mode: Encoding text into image")
+        # Check file: or str:
+        if message[:4] == "str:":
+            mType = True
+            message = message[4:]
+        elif message[:5] == "file:":
+            mType = False
+            message = message[5:]
+        else:
+            logging.error("Bad message provided")
+            sys.exit(1)
+        encodedImage = encode_text(startImage, message, mType)
         cv2.imwrite(outFile, encodedImage)
+        return outFile
 
-    elif file_to_hide != None:
-        logging.debug("Mode: Hiding file into image")
-        encode_file(startImage, file_to_hide)
-
-    elif message == None and file_to_hide == None:
-        logging.debug("Mode: Unhiding message/file from image")
-        # TODO decide on how to handle text and file decoding
-        decodedText = decode_text()
-        decode_file()
-
+    elif not mode:
+        # Unhide message from image
+        logging.debug("Mode: Decoding message/file from image")
+        decodedText, mType = decode_text(startImage)
+        return decodedText, mType
